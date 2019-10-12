1f9310d5139da9884a64d0569450e8820d8702bb
Rebeca Kitchen <42246509+rebecamkitchen@users.noreply.github.com>
2019-10-12 14:36:46 -0500

    Delete webscraping.py

--- webscraping.py
+++
@@ -1,9 +0,0 @@
-# https://www.airbnb.com/robots.txt
-# anywebsite/robots.txt will tell you what data you are/are not allowed to scrape
-
-# Get more familiar with BeautifulSoup
-# Use APIs - some websites provide these for developers
-
-# Look at Scrapy Framework (larger than beautiful soup)
-# Use a database to store collected data and access later
-
