fc5c1b38d6f46ef651468f338057cd81a059195a
Rebeca Kitchen <42246509+rebecamkitchen@users.noreply.github.com>
2019-10-12 14:35:52 -0500

    Add file via upload

---
+++ webscraping.py
@@ -0,0 +1,9 @@
+# https://www.airbnb.com/robots.txt
+# anywebsite/robots.txt will tell you what data you are/are not allowed to scrape
+
+# Get more familiar with BeautifulSoup
+# Use APIs - some websites provide these for developers
+
+# Look at Scrapy Framework (larger than beautiful soup)
+# Use a database to store collected data and access later
+
